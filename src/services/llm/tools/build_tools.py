4b0958fde30700553d9482b29fc8332abd73c5f3
Emmanuel Nnadi <ennadi@my.dom.edu>
2024-04-09 00:07:35 -0500

    added the joke list

--- src/services/llm/tools/build_tools.py
+++ src/services/llm/tools/build_tools.py
@@ -1,6 +1,20 @@
 from src.services.llm.tools.say_hi import say_hi_in_italian
+from langchain_core.tools import tool
+from datetime import datetime
 
+# to do:  tell jokes.
+# Situation: The user asks the Virtual Agent to tell a joke and the Virtual Agent should respond back in Spanish
+def Jokes_in_spanish(txt: str) -> str:
+    """Use this tool when you understand the user is asking for a joke and tell a joke back in spanish"""
 
-
+    jokes_list = ["¿Por qué los pájaros no usan Facebook? Porque ya tienen Twitter.",
+                  "¿Cuál es el animal más antiguo? La cebra, porque está en blanco",
+                  "¿Qué le dice un gusano a otro gusano? No me mires, soy yo",]
+    
+    time = datetime.now()
+    seconds = time.second
+    joke = jokes_list[seconds % len(jokes_list)]
+    return joke
+# Situation: The user asks the Virtual Agent to tell a joke and the Virtual Agent should respond back in Italian
 
 tools = [say_hi_in_italian]
